e208d28a1f8126035d09d54ae9b0d89285a78ebb
Your Name <you@example.com>
2026-01-19 19:47:49 +0500

    main

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import multishop from "./../public/photo_2025-11-29_22-01-35.jpg"
+import organick from "./../public/Copilot_20251129_221520.png"
+
+const App = () => {
+  return (
+    <div>
+      <img src={multishop} alt="" />
+      <img src={organick} alt="" />
+    </div>
+  )
+}
+
+export default App
