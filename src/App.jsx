f4557c3f049d72b14545b8d8655b4802fb8db19a
Your Name <you@example.com>
2026-01-19 23:00:42 +0500

    2upd

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import multishop from "./../public/photo_2025-11-29_22-01-35.jpg"
 import organick from "./../public/Copilot_20251129_221520.png"
 import husan from "./../public/image.png"
+import husancha from "./../public/image copy.png"
+import hasan from "./../public/image copy 2.png"
+import umar from "./../public/image copy 3.png"
+import saidamin from "./../public/image copy 4.png"
+import  husanpesh from "./../public/image copy 5.png"
+import ikrom from "./../public/image copy 6.png"
+import diyor from "./../public/image copy 7.png"
 
 const App = () => {
   return (
@@ -9,6 +16,13 @@ const App = () => {
       <img src={multishop} alt="" />
       <img src={organick} alt="" />
       <img src={husan} alt="" />
+      <img src={husancha} alt="" />
+      <img src={hasan} alt="" />
+      <img src={umar} alt="" />
+      <img src={saidamin} alt="" />
+      <img src={husanpesh} alt="" />
+      <img src={ikrom} alt="" />
+      <img src={diyor} alt="" />
     </div>
   )
 }
